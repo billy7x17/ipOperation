e0c75a6dd966145f3c242b906e05026d044768b7
billy119 <billy7x17@gmail.com>
2015-02-15 15:17:11 +0800

    添加注释

--- MainTest/src/com/billytest/ip/IpSegmentUtil.java
+++ MainTest/src/com/billytest/ip/IpSegmentUtil.java
@@ -3,75 +3,107 @@ package com.billytest.ip;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * ip段工具类
+ * @author <a href="mailto:li-zr@neusoft.com">Zongrui Li</a>
+ * @version $Revision 1.0 $ 2015年2月15日 下午2:59:46
+ */
 public class IpSegmentUtil {
 
-	public static String getMask(String ipSegment) throws MaskErrorException {
+    /**
+     * getMask 根据IP段获取子网掩码
+     * @param ipSegment ip段，格式为192.168.1.1/24
+     * @return 子网掩码
+     * @throws MaskErrorException 若掩码位数不合法，会抛出异常
+     */
+    public static String getMask(String ipSegment) throws MaskErrorException {
 
-		String maskNum = ipSegment.split("/")[1];
-		
-		if (Integer.valueOf(maskNum) < 1 || Integer.valueOf(maskNum) > 32) {
-			throw new MaskErrorException();
-		}
+        String maskNum = ipSegment.split("/")[1];
 
-		long mask = ((((long) Math.pow(2D, Double.valueOf(maskNum))) - 1L) << (32 - Integer
-				.valueOf(maskNum)));
+        if (Integer.valueOf(maskNum) < 1 || Integer.valueOf(maskNum) > 32) {
+            throw new MaskErrorException();
+        }
 
-		return IpTransUtil.dec2Ip(mask);
-	}
+        long mask = ((((long) Math.pow(2D, Double.valueOf(maskNum))) - 1L) << (32 - Integer
+                .valueOf(maskNum)));
 
-	public static List<String> getExtremeIp(String ipSegment) throws MaskErrorException {
+        return IpTransUtil.dec2Ip(mask);
+    }
 
-		String seg[] = ipSegment.split("/");
+    /**
+     * getExtremeIp 根据ip段获取首尾IP
+     * @param ipSegment ip段，格式为192.168.1.1/24
+     * @return 由首位IP组成的list，list[0]为首IP，list[1]为尾IP
+     * @throws MaskErrorException 若掩码位数不合法，会抛出异常
+     */
+    public static List<String> getExtremeIp(String ipSegment) throws MaskErrorException {
 
-		String ip = seg[0];
+        String seg[] = ipSegment.split("/");
 
-		String mask = getMask(ipSegment);
+        String ip = seg[0];
 
-		Long ipLong = IpTransUtil.ip2Dec(ip);
+        String mask = getMask(ipSegment);
 
-		Long maskLong = IpTransUtil.ip2Dec(mask);
+        Long ipLong = IpTransUtil.ip2Dec(ip);
 
-		Long last = ipLong | ((Long.lowestOneBit(maskLong) - 1));
+        Long maskLong = IpTransUtil.ip2Dec(mask);
 
-		Long first = (ipLong & maskLong);
+        Long last = ipLong | ((Long.lowestOneBit(maskLong) - 1));
 
-		List<String> list = new ArrayList<String>();
+        Long first = (ipLong & maskLong);
 
-		String firstIp = String.valueOf(IpTransUtil.dec2Ip(first));
-		String lastIp = String.valueOf(IpTransUtil.dec2Ip(last));
+        List<String> list = new ArrayList<String>();
 
-		if (firstIp.endsWith(".0"))
-			firstIp = firstIp.replaceAll("0$", "1");
+        String firstIp = String.valueOf(IpTransUtil.dec2Ip(first));
+        String lastIp = String.valueOf(IpTransUtil.dec2Ip(last));
 
-		if (lastIp.endsWith(".255"))
-			lastIp = lastIp.replaceAll("255$", "254");
+        if (firstIp.endsWith(".0")) firstIp = firstIp.replaceAll("0$", "1");
 
-		list.add(firstIp);
-		list.add(lastIp);
+        if (lastIp.endsWith(".255")) lastIp = lastIp.replaceAll("255$", "254");
 
-		return list;
-	}
+        list.add(firstIp);
+        list.add(lastIp);
 
-	public static Integer getIpSegmentSize(String firstIp, String lastIp) {
+        return list;
+    }
 
-		return getIpList(firstIp, lastIp).size();
+    /**
+     * getIpSegmentSize 根据IP段首尾IP获取IP段所有IP个数
+     * @param firstIp 首位IP
+     * @param lastIp 尾位IP
+     * @return IP段里所有IP个数
+     */
+    public static Integer getIpSegmentSize(String firstIp, String lastIp) {
 
-	}
+        return getIpList(firstIp, lastIp).size();
 
-	public static List<String> getIpList(String first, String last) {
+    }
 
-		Long firstIp = IpTransUtil.ip2Dec(first);
-		Long lastIp = IpTransUtil.ip2Dec(last);
+    /**
+     * getIpList 根据首尾IP获取所有IPlist
+     * @param first 首位IP
+     * @param last 尾位IP
+     * @return IP段里所有IP的list
+     */
+    public static List<String> getIpList(String first, String last) {
 
-		List<String> ipList = new ArrayList<String>();
+        Long firstIp = IpTransUtil.ip2Dec(first);
+        Long lastIp = IpTransUtil.ip2Dec(last);
 
-		for (long _current_ip = firstIp; _current_ip <= lastIp; _current_ip++) {
-			String ip = IpTransUtil.dec2Ip(_current_ip);
-			if (!ip.endsWith(".0") && !ip.endsWith(".255"))
-				ipList.add(ip);
-		}
+        if (firstIp > lastIp) {
+            Long tmp = firstIp;
+            firstIp = lastIp;
+            lastIp = tmp;
+        }
 
-		return ipList;
-	}
+        List<String> ipList = new ArrayList<String>();
+
+        for (long _current_ip = firstIp; _current_ip <= lastIp; _current_ip++) {
+            String ip = IpTransUtil.dec2Ip(_current_ip);
+            if (!ip.endsWith(".0") && !ip.endsWith(".255")) ipList.add(ip);
+        }
+
+        return ipList;
+    }
 
 }
--- MainTest/src/com/billytest/ip/IpTransUtil.java
+++ MainTest/src/com/billytest/ip/IpTransUtil.java
@@ -1,43 +1,57 @@
 package com.billytest.ip;
 
+/**
+ * IP字符串与十进制长整型数据类型转换的工具类
+ * @author <a href="mailto:li-zr@neusoft.com">Zongrui Li</a>
+ * @version $Revision 1.0 $ 2015年2月15日 下午3:13:04
+ */
 class IpTransUtil {
 
-	public static Long ip2Dec(String ip) {
+    /**
+     * ip2Dec 字符串转换为长整型
+     * @param ip IP字符串 格式为xxx.xxx.xxx.xxx
+     * @return 长整型的IP
+     */
+    public static Long ip2Dec(String ip) {
 
-		String[] ss = ip.split("\\.");
+        String[] ss = ip.split("\\.");
 
-		Long result = 0L;
+        Long result = 0L;
 
-		for (int i = 0; i < ss.length; i++) {
-			Long t = Long.valueOf(ss[i]);
+        for (int i = 0; i < ss.length; i++) {
+            Long t = Long.valueOf(ss[i]);
 
-			Long tmp = t << (3 - i) * 8L;
+            Long tmp = t << (3 - i) * 8L;
 
-			result += tmp;
-		}
+            result += tmp;
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	public static String dec2Ip(Long dec) {
+    /**
+     * dec2Ip 长整型转换为字符串
+     * @param dec 长整型十进制IP
+     * @return 格式为xxx.xxx.xxx.xxx的字符串
+     */
+    public static String dec2Ip(Long dec) {
 
-		String result = "";
+        String result = "";
 
-		Long tmp = new Long(0L);
+        Long tmp = new Long(0L);
 
-		for (int i = 3; i >= 0; i--) {
+        for (int i = 3; i >= 0; i--) {
 
-			tmp = (dec / (1 << 8L * i));
+            tmp = (dec / (1 << 8L * i));
 
-			result += tmp;
+            result += tmp;
 
-			dec -= (tmp << 8L * i);
+            dec -= (tmp << 8L * i);
 
-			if (i != 0)
-				result += ".";
-		}
+            if (i != 0) result += ".";
+        }
 
-		return result;
-	}
+        return result;
+    }
 
 }
